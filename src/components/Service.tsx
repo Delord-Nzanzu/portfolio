1dbb2d3962101f9523dd3cdfd48669fc1030eb05
Delord-Nzanzu <wayirenzanzu@gmail.com>
2025-04-02 12:46:02 +0200

    feat:ajout de icon dans service

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -1,16 +1,25 @@
 import { Container, Divider, Grid2, Typography } from "@mui/material";
 import HMenu from "./HMenu";
+import {
+  DesktopMac,
+  DesktopWindows,
+  PhoneAndroid,
+  VpnKeySharp,
+  Web,
+} from "@mui/icons-material";
 
 interface service {
-  id: number;
+  id: number | string;
   text: string;
   descr: React.ReactNode;
+  icon?: React.ReactNode;
 }
 
 function Service() {
   const data: service[] = [
     {
-      id: 1,
+      id: "01",
+      icon: <DesktopWindows sx={{ color: "#00FF99", fontSize: 100 }} />,
       text: "Développement Front-End",
       descr: (
         <>
@@ -23,8 +32,9 @@ function Service() {
       ),
     },
     {
-      id: 2,
+      id: "02",
       text: "Développement Back-End",
+      icon: <DesktopMac sx={{ color: "#00FF99", fontSize: 100 }} />,
       descr: (
         <>
           Conception et développement de solutions robustes avec{" "}
@@ -35,8 +45,9 @@ function Service() {
       ),
     },
     {
-      id: 3,
+      id: "03",
       text: "Développement Mobile",
+      icon: <PhoneAndroid sx={{ color: "#00FF99", fontSize: 100 }} />,
       descr: (
         <>
           Développement d'interfaces mobiles modernes et performantes avec
@@ -48,8 +59,9 @@ function Service() {
       ),
     },
     {
-      id: 4,
+      id: "04",
       text: "Déploiement des application",
+      icon: <VpnKeySharp sx={{ color: "#00FF99", fontSize: 100 }} />,
       descr: (
         <>
           Déploiement d'applications sur un{" "}
@@ -125,12 +137,13 @@ function Service() {
                   sx={{
                     fontSize: 50,
                     fontFamily: "Courier Prime",
+                    textAlign: "center",
                     color: "#fff",
                     ":hover": {
                       color: "#00FF99",
                     },
                   }}>
-                  {e.id}
+                  {e.icon}
                 </Typography>
                 <Typography
                   sx={{
